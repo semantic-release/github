5585c3b64b2be84525ac58b364ae8acccd0882af
babblebey <babblebey@gmail.com>
2024-06-14 18:53:09 +0100

    do: address test case `Editing the release to include all release links at the bottom`

--- test/success.test.js
+++ test/success.test.js
@@ -1445,14 +1445,19 @@ test("Editing the release to include all release links at the bottom", async (t)
     .getOnce(`https://api.github.local/repos/${owner}/${repo}`, {
       full_name: `${owner}/${repo}`,
     })
-    .getOnce(
-      `https://api.github.local/search/issues?q=${encodeURIComponent(
-        `repo:${owner}/${repo}`,
-      )}+${encodeURIComponent("type:pr")}+${encodeURIComponent(
-        "is:merged",
-      )}+${commits.map((commit) => commit.hash).join("+")}`,
-      { items: prs },
-    )
+    .postOnce("https://api.github.local/graphql", {
+      data: {
+        repository: {
+          commit123: {
+            associatedPullRequests: {
+              nodes: [
+                prs[0]
+              ],
+            },
+          }
+        },
+      },
+    })
     .getOnce(
       `https://api.github.local/repos/${owner}/${repo}/pulls/1/commits`,
       [{ sha: commits[0].hash }],
