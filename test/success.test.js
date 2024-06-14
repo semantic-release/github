b7963d79e7572edb69dc316c6e553b5b9306d0ec
babblebey <babblebey@gmail.com>
2024-06-14 18:40:56 +0100

    do: address test case `Do not add comment and labels to PR/issues from other repo`

--- test/success.test.js
+++ test/success.test.js
@@ -886,14 +886,27 @@ test("Do not add comment and labels to PR/issues from other repo", async (t) => 
     .getOnce(`https://api.github.local/repos/${owner}/${repo}`, {
       full_name: `${owner}/${repo}`,
     })
-    .getOnce(
-      `https://api.github.local/search/issues?q=${encodeURIComponent(
-        `repo:${owner}/${repo}`,
-      )}+${encodeURIComponent("type:pr")}+${encodeURIComponent(
-        "is:merged",
-      )}+${commits.map((commit) => commit.hash).join("+")}`,
-      { items: [] },
-    )
+    .postOnce("https://api.github.local/graphql", {
+      data: {
+        repository: {
+          commit123: {
+            associatedPullRequests: {
+              nodes: [],
+            },
+          },
+          commit456: {
+            associatedPullRequests: {
+              nodes: [],
+            },
+          },
+          commit789: {
+            associatedPullRequests: {
+              nodes: [],
+            },
+          }
+        },
+      },
+    })
     .postOnce(
       `https://api.github.local/repos/${owner}/${repo}/issues/2/comments`,
       { html_url: "https://github.com/successcomment-2" },
